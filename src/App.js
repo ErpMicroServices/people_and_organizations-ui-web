221ebfb121f1d881091c5f573d00fc2ded955dab
Jim Barrows <Jim.Barrows@gmail.com>
2020-06-22 09:46:28 -0500

    Can pull party types from the docker compose images just fine.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
-import React, {Component} from 'react';
-import './App.css';
-import logo from './logo.svg';
+import React, {Component} from 'react'
+import './App.css'
+import logo               from './logo.svg'
 
 class App extends Component {
-	render() {
+
+	componentDidMount = async () => {
+		let response   = await fetch("partyTypes/")
+		let partyTypes = await response.json()
+		console.log('partyTypes: ', partyTypes)
+		this.setState(partyTypes)
+	}
+
+	render () {
+		if (this.state && this.state._embedded) {
+			console.log(this.state._embedded)
+		}
+
 		return (
-				<div className="App">
-					<header className="App-header">
-						<img src={logo} className="App-logo" alt="logo"/>
-						<h1 className="App-title">Welcome to React</h1>
-					</header>
-					<p className="App-intro">
-						To get started, edit <code>src/App.js</code> and save to reload.
-					</p>
-				</div>
-		);
+			<div className="App" >
+				<header className="App-header" >
+					<img src={logo} className="App-logo" alt="logo" />
+					<h1 className="App-title" >Welcome to React</h1 >
+				</header >
+				<p className="App-intro" >
+					To get started, edit <code >src/App.js</code > and save to reload.
+				</p >
+				<ul >
+					{this.state && this.state._embedded ? this.state._embedded.partyTypes.map(p =>
+																																											<li >{p.description}</li >) : ""}
+				</ul >
+			</div >
+		)
 	}
 }
 
-export default App;
+export default App
