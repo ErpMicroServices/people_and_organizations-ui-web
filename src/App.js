96e5b77fedf6b5d519df400cb41ef2b5b081840f
Jim Barrows <Jim.Barrows@gmail.com>
2020-12-23 09:58:19 -0600

    Moved party types into it's own directory.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React, {Component} from 'react'
 import './App.css'
 import logo               from './logo.svg'
+import Party_types        from './party_types'
 
 class App extends Component {
 
-	componentDidMount = async () => {
-		let response   = await fetch("/people_and_organizations/api/partyTypes")
-		let partyTypes = await response.json()
-		this.setState({
-										partyTypes: partyTypes._embedded.partyTypes,
-										page      : partyTypes.page
-									})
-	}
+
 
 	render () {
 		return (
@@ -23,24 +17,11 @@ class App extends Component {
 				<p className="App-intro" >
 					To get started, edit <code >src/App.js</code > and save to reload.
 				</p >
-				<h1 >Party Types</h1 >
-				<ul >
-					{this.state.partyTypes.map((p, i) =>
-																			 <li key={i} ><a href={p._links.self.href} >{p.description}</a ></li >)}
-				</ul >
+				<Party_types />
 			</div >
 		)
 	}
 
-	state = {
-		partyTypes: [],
-		"page"    : {
-			"size"         : 0,
-			"totalElements": 0,
-			"totalPages"   : 0,
-			"number"       : 0
-		}
-	}
 }
 
 export default App
---
+++ src/party_types/index.jsx
@@ -0,0 +1,45 @@
+import PropTypes from 'prop-types'
+import React     from 'react'
+
+export default class index extends React.Component {
+
+	static propTypes = {
+		id: PropTypes.string.isRequired
+	}
+
+	static defaultProps = {}
+
+	componentDidMount = async () => {
+		let response   = await fetch("/people_and_organizations/api/partyTypes")
+		let partyTypes = await response.json()
+		this.setState({
+										partyTypes: partyTypes._embedded.partyTypes,
+										page      : partyTypes.page
+									})
+	}
+
+	render () {
+		const componentName = 'indexComponent'
+		const id            = `${componentName}_${this.props.id}`
+		return (
+			<div id={id} className={componentName} >
+				<h1 >Party Types</h1 >
+				<ul >
+					{this.state.partyTypes.map((p, i) =>
+																			 <li key={i} ><a href={p._links.self.href} >{p.description}</a ></li >)}
+				</ul >
+			</div >
+		)
+	}
+
+	state = {
+		partyTypes: [],
+		"page"    : {
+			"size"         : 0,
+			"totalElements": 0,
+			"totalPages"   : 0,
+			"number"       : 0
+		}
+	}
+}
+
