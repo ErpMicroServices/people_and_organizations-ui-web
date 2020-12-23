8e7530ec058706bad04f416f66897b6e4dd2f9b1
Jim Barrows <Jim.Barrows@gmail.com>
2020-12-23 09:51:43 -0600

    Installed and got Traefik working for proxying.

--- src/App.js
+++ src/App.js
@@ -5,17 +5,15 @@ import logo               from './logo.svg'
 class App extends Component {
 
 	componentDidMount = async () => {
-		let response   = await fetch("partyTypes/")
+		let response   = await fetch("/people_and_organizations/api/partyTypes")
 		let partyTypes = await response.json()
-		console.log('partyTypes: ', partyTypes)
-		this.setState(partyTypes)
+		this.setState({
+										partyTypes: partyTypes._embedded.partyTypes,
+										page      : partyTypes.page
+									})
 	}
 
 	render () {
-		if (this.state && this.state._embedded) {
-			console.log(this.state._embedded)
-		}
-
 		return (
 			<div className="App" >
 				<header className="App-header" >
@@ -25,12 +23,23 @@ class App extends Component {
 				<p className="App-intro" >
 					To get started, edit <code >src/App.js</code > and save to reload.
 				</p >
+				<h1 >Party Types</h1 >
 				<ul >
-					{this.state && this.state._embedded ? this.state._embedded.partyTypes.map(p =>
-																																											<li >{p.description}</li >) : ""}
+					{this.state.partyTypes.map((p, i) =>
+																			 <li key={i} ><a href={p._links.self.href} >{p.description}</a ></li >)}
 				</ul >
 			</div >
 		)
+	}
+
+	state = {
+		partyTypes: [],
+		"page"    : {
+			"size"         : 0,
+			"totalElements": 0,
+			"totalPages"   : 0,
+			"number"       : 0
+		}
 	}
 }
 
