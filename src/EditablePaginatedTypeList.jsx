d5e133a1bc9acf0a0ece54756b6dd327376334f2
Jim Barrows <Jim.Barrows@gmail.com>
2021-02-13 12:05:01 -0600

    Added the pagination control.

--- src/EditablePaginatedTypeList.jsx
+++ src/EditablePaginatedTypeList.jsx
@@ -1,9 +1,8 @@
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import PropTypes         from 'prop-types'
 import React             from 'react'
 
 import './EditablePaginatedTypeList.css'
-import NumberField       from './NumberField'
+import PaginationControl from './PaginationControl'
 
 export default class PaginatedList extends React.Component {
 
@@ -15,34 +14,15 @@ export default class PaginatedList extends React.Component {
 
 	static defaultProps = {}
 
-	renderButton = pageNumber => <button id={`$PaginatedListComponent_button_${pageNumber}`}
-		onClick={e => this.setState({page: pageNumber})} >
-		{pageNumber + 1}
-	</button >
-
 	render () {
 		const componentName = 'PaginatedListComponent'
 		const id            = `${componentName}_${this.props.id}`
 		const foo           = this
 		return <div id={id} className={componentName} >
-			<div id={`${id}_controls`} >
-				<button id={`${id}_beginning`} onClick={e => this.setState({page: 1})} >
-					<FontAwesomeIcon icon={'angle-double-left'} />
-				</button >
-				<button id={`${id}_next`} onClick={e => this.setState({page: this.state.page - 1})} >
-					<FontAwesomeIcon icon={'angle-left'} />
-				</button >
-				{[...Array(this.state.result_page.totalPages).keys()]
-					.map(pageNumber => this.renderButton(pageNumber))}
-				<button id={`${id}_beginning`} onClick={e => this.setState({page: this.state.page + 1})} >
-					<FontAwesomeIcon icon={'angle-right'} />
-				</button >
-				<button id={`${id}_beginning`} onClick={e => this.setState({page: this.state.result_page.totalPages})} >
-					<FontAwesomeIcon icon={'angle-double-right'} />
-				</button >
-			</div >
-			<NumberField id={'size'} label={'Size'} value={this.state.size}
-				onChange={e => this.setState({size: e.target.value, page: 1})} />
+			<PaginationControl id={id} result_page={this.state.result_page} page={this.state.page} size={this.state.size}
+				onPageNumberChange={pageNumber => this.setState({page: pageNumber})}
+				onPageSizeChange={pageSize => this.setState({size: pageSize, page: 1})} />
+
 			<div id={`${id}_data`} >
 				<ul >
 					{this.state.list.map((element, index) => <li key={index} >
---
+++ src/PaginationControl/index.jsx
@@ -0,0 +1,65 @@
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import PropTypes         from 'prop-types'
+import React             from 'react'
+import NumberField       from '../NumberField'
+
+export default class index extends React.Component {
+
+	static propTypes = {
+		id                : PropTypes.string.isRequired,
+		page              : PropTypes.number.isRequired,
+		size              : PropTypes.number.isRequired,
+		result_page       : PropTypes.shape({
+																					size         : PropTypes.number.isRequired,
+																					totalElements: PropTypes.number.isRequired,
+																					totalPages   : PropTypes.number.isRequired,
+																					number       : PropTypes.number.isRequired
+																				}).isRequired,
+		onPageNumberChange: PropTypes.func.isRequired,
+		onPageSizeChange  : PropTypes.func.isRequired
+	}
+
+	static defaultProps = {
+		result_page: {
+			size         : 0,
+			totalElements: 0,
+			totalPages   : 0,
+			number       : 0
+		},
+		page       : 1,
+		size       : 20,
+	}
+
+	renderButton = pageNumber => <button id={`$PaginatedListComponent_button_${pageNumber}`}
+		onClick={e => this.setState({page: pageNumber})} >
+		{pageNumber + 1}
+	</button >
+
+	render () {
+		const componentName = 'PaginationControlComponent'
+		const id            = `${componentName}_${this.props.id}`
+		return (
+			<div id={id} className={componentName} >
+				<button id={`${id}_beginning`} onClick={e => this.props.onPageNumberChange(1)} >
+					<FontAwesomeIcon icon={'angle-double-left'} />
+				</button >
+				<button id={`${id}_next`} onClick={e => this.props.onPageNumberChange(this.props.page - 1)} >
+					<FontAwesomeIcon icon={'angle-left'} />
+				</button >
+				{[...Array(this.props.result_page.totalPages).keys()]
+					.map(pageNumber => this.renderButton(pageNumber))}
+				<button id={`${id}_beginning`} onClick={e => this.props.onPageNumberChange(this.props.page + 1)} >
+					<FontAwesomeIcon icon={'angle-right'} />
+				</button >
+				<button id={`${id}_beginning`} onClick={e => this.props.onPageNumberChange(this.props.result_page.totalPages)} >
+					<FontAwesomeIcon icon={'angle-double-right'} />
+				</button >
+				<NumberField id={'size'} label={'Size'} value={this.props.size}
+					onChange={e => this.props.onPageSizeChange(e.target.value)} />
+			</div >
+		)
+	}
+
+	state = {}
+}
+
