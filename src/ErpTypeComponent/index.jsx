ee4d817d71307abf32179ced4171fab1dfcf131c
Jim Barrows <Jim.Barrows@gmail.com>
2024-02-08 22:12:35 -0600

    various changes

--- src/ErpTypeComponent/index.jsx
+++ src/ErpTypeComponent/index.jsx
@@ -3,45 +3,70 @@ import React, {useEffect, useState} from 'react'
 import {useForm} from "react-hook-form";
 
 
+function emptyHateaosRecord(typename) {
+    let emptyHateoasResponse = {_embedded: {}};
+    emptyHateoasResponse._embedded[typename] = [];
+    return emptyHateoasResponse;
+}
+
+function getUri(uri) {
+    return fetch(uri, {
+        headers: {
+            'Accept': 'application/hal+json',
+        }
+    })
+        .then(response => response.json());
+}
+
+async function deleteUri(type) {
+    await fetch(type._links.self.href,
+        {
+            method: 'DELETE',
+            headers: {
+                'Accept': 'application/hal+json',
+                'Content-Type': 'application/hal+json'
+            },
+            body: null
+        }
+    );
+}
+
 function ErpTypeCommponent(props) {
     const {id, componentName, title, typename, uri} = props;
     const {
         register,
         handleSubmit,
-        watch,
+        reset,
         formState: {errors},
     } = useForm();
 
-    let emptyHateoasResponse = {_embedded: {}};
-    emptyHateoasResponse._embedded[typename] = [];
-
-    const [hateoas, setHateoas] = useState(emptyHateoasResponse);
+    const [hateoas, setHateoas] = useState(emptyHateaosRecord(typename));
 
     useEffect(() => {
-        fetch(uri, {
-            headers: {
-                'Accept': 'application/json',
-            }
-        })
-            .then(response => response.json())
+        getUri(uri)
             .then(data => setHateoas(data))
             .catch(error => console.error(error));
     }, []);
 
+    const onDeleteButtonClick = async (type, e) => {
+        e.preventDefault();
+        await deleteUri(type);
+    }
     const onSubmit = async (data) => {
         await fetch(uri,
             {
-                method: "post",
+                method: "POST",
                 body: JSON.stringify(data),
                 headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
+                    'Accept': 'application/hal+json',
+                    'Content-Type': 'application/hal+json'
                 }
             })
             .then(response => response.json())
             .then(data => {
                 hateoas._embedded[typename].push(data);
-                setHateoas(hateoas)
+                setHateoas(hateoas);
+                reset();
             })
     };
 
@@ -54,13 +79,22 @@ function ErpTypeCommponent(props) {
         </form>
         {
             hateoas._embedded[typename].length ? (
-                <ul>
+                <table>
+                    <tbody>
                     {
-                        hateoas._embedded[typename].map(type => (
-                            <li id={type.id} key={type.description}>{type.description}</li>
+                        hateoas._embedded[typename].map((type, index) => (
+                            <tr key={index}>
+                                <td>
+                                    <button type={"button"}
+                                            onClick={(e) => onDeleteButtonClick(type, e)}>Delete
+                                    </button>
+                                </td>
+                                <td id={type.id} key={type.description}>{type.description}</td>
+                            </tr>
                         ))
                     }
-                </ul>
+                    </tbody>
+                </table>
             ) : <p>There are no types</p>
         }
     </div>
